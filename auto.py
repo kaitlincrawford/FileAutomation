880ccd934aa677a08ed7972c2cccc401bf38d3e1
kmejia-crawford <kmejia-crawford@unomaha.edu>
2024-03-16 16:43:13 -0500

    Update auto.py

--- auto.py
+++ auto.py
@@ -6,7 +6,7 @@ def open_folder_in_explorer(folder_path):
     # Open folder in File Explorer
     subprocess.Popen(f'explorer "{folder_path}"')
 
-# Example usage
+# Change folder path to desired path
 folder_path = r'C:\folder_path'
 open_folder_in_explorer(folder_path)
 
